2189ffcf1a63c8a489f5f8e1a43cb91f621867a4
Bahar Teke <rahabeket@gmail.com>
2023-12-12 05:24:13 +0300

    B4

---
+++ src/main/java/git/Account.java
@@ -0,0 +1,12 @@
+package git;
+
+public class Account {
+    public static void main(String[] args) {
+        System.out.println("bahar");
+        //B2
+        System.out.println("merve");
+        //  B3
+        System.out.println("emir");
+        //B4
+    }
+}
